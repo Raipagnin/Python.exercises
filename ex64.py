81716b3c49936f9d579528c6f2a2facfb140b36a
Rai Pagin <64612453+Raipagnin@users.noreply.github.com>
2020-05-03 10:46:34 +0000

    ex64
    somando valores adicionados sem a flag de 999 para parar

--- ex64.py
+++ ex64.py
@@ -1,1 +1,9 @@
-print('{:-^50}'.format(' SEQUENCIA FIBONACCI '))
+#com a aula 15 vc consegue resolver um problema daqui melhor
+print('{:-^50}'.format(' Tratando de valores '))
+num = cont= soma = 0
+while num != 999:
+    num = int(input('Digite um numero [999] para parar: '))
+    soma += num
+    cont += 1
+print('Vc digitou {} numeros e a soma entre eles eh {}'.format(cont-1, soma-999))
+print('FIM')
