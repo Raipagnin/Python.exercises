024e7968a6d49e21d0f3ed58363b664ec6ee3ddc
Rai Pagin <64612453+Raipagnin@users.noreply.github.com>
2020-05-08 13:43:36 +0000

    cheat code for colours
    tabelinha de cores. cheat code

--- cores.py
+++ cores.py
@@ -10,3 +10,12 @@ y = int(input('Digite a sua idade: '))
 if y >= 20:
     print('Mto prazer, {}{}{}! Nao queria te dizer mas vc eh mto {}velho{}!'.format(cor['blu'], x, cor['limpa'],
                                                                                     cor['red'], cor['limpa']))
+'''style for font:0, 1, 4, 7
+0 - none, regular
+1 - bold
+4 - underline
+7 - negative'''
+''' background colour follows regular colour by difference of decimal number 40 instead of 30
+ex: font colour 30 = white, background colour 40 = white
+30/40 = white    31/41 = red      32/42 = green      33/43 = yellow       34/44 = blue
+35/45 = magenta(purple)     36/46 = cian         37/47 = grey
